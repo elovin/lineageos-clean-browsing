d4302ad1d9a18b982454c70ae79ceaed99028b57
elovin <32832123+elovin@users.noreply.github.com>
2023-09-27 12:05:52 +0200

    feat: Run ip range whitelist for thethering in background
    also change ip ranges to short notation

--- allow_ips_from_allowedlist_for_thethering.sh
+++ allow_ips_from_allowedlist_for_thethering.sh
@@ -18,7 +18,7 @@ resolve_ip() {
 }
 
 # Read each domain from the whitelist file using while-read loop, ignoring lines starting with '#' and empty lines
-lookup_and_prepend_ips_from_allowedlist_to_forwardchain(){
+lookup_and_prepend_ips_based_on_dns_allowedlist_to_forwardchain(){
     while IFS= read -r line; do
         # Remove leading and trailing whitespace using BusyBox awk
         line=$(echo "$line" | /system/bin/busybox awk '{$1=$1};1')
@@ -35,9 +35,9 @@ lookup_and_prepend_ips_from_allowedlist_to_forwardchain(){
         ip=$(resolve_ip "$line")
     
         if [ -n "$ip" ]; then
-            # Allow new IP address in iptables forwarding chain
-            /system/bin/iptables -I "$FORWARDING_CHAIN" -d "$ip/255.255.255.0" -j ACCEPT
-            echo "Allowed IP: $ip"
+            # Prepend new IP address range to iptables forwarding chain
+            /system/bin/iptables -I "$FORWARDING_CHAIN" -d "$ip/24" -j ACCEPT
+            echo "Allowed Subnet: '$ip/24'"
         else
             echo "Failed to resolve IP for domain: $line"
         fi
@@ -52,8 +52,10 @@ main(){
     fi
 
     while true; do
-        lookup_and_prepend_ips_from_allowedlist_to_forwardchain
-        /system/bin/sleep 600  # Sleep for 10 minutes (600 seconds)
+        # looup and prepend ip ranges, continue on error, run in background subshell
+        (lookup_and_prepend_ips_based_on_dns_allowedlist_to_forwardchain || true) &
+        # Sleep for 10 minutes (600 seconds)
+        /system/bin/sleep 600
     done
 }
 
