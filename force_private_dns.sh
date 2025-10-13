f1df45c04f8a4e39e467f74f648bdcee7ab68774
elovin <32832123+elovin@users.noreply.github.com>
2025-10-13 19:35:32 +0200

    Update force_private_dns.sh

--- force_private_dns.sh
+++ force_private_dns.sh
@@ -2,10 +2,11 @@
 
 sleep 5;
 
+# this setting only works after boot has been completed
 pm set-user-restriction --user 0 no_config_vpn 1
 
 while :; do
-	# disable access to vpn settings
+	# reset private dns every 5 seconds
 	settings put global private_dns_mode hostname
 	settings put global private_dns_specifier "$(cat /system/etc/private_dns_name.txt)"
 
